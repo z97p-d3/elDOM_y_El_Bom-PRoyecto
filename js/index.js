8b861724d8de943db94886884a8e7aafa8f2973a
Dario Pilacuan <dario.pilacuan@epn.edu.ec>
2018-10-23 22:44:12 -0500

    Initial commit

---
+++ js/index.js
@@ -0,0 +1,14 @@
+//mpostrar la tabal de datos de JSON estudiantes
+
+document.getElementById("mostrar-datos").onclick = function () {
+
+	var dat = document.getElementById("datos");
+
+	if (dat.style.display == "none") {
+		dat.style.display = "block"
+	} else {
+
+		dat.style.display = "none"
+	}
+
+}
